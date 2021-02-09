cdd78db1da0ac97ecd032b7ee9c281a25ca562f9
ArmeetJatyani <armeetjatyani@gmail.com>
2021-02-08 17:46:37 -0800

    player movement and chasing AI complete!

---
+++ unity-helloworld/Assets/AI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+
+    public float speed = 7;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 displacementFromTarget = target.position - transform.position;
+        Vector3 directionToTarget = displacementFromTarget.normalized;
+
+        Vector3 velocity = directionToTarget * speed;
+
+        float distToTarget = displacementFromTarget.magnitude;
+        if (distToTarget > 3f)
+        {
+            transform.Translate(velocity * Time.deltaTime);
+        }
+
+    }
+}
